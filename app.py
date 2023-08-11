bbac8893c60ee8aa587613523b4b973f609652d2
Tabares1 <alejotabares1902@gmail.com>
2023-08-11 09:16:06 -0500

    Se resuelve el ejercicio 1

---
+++ app.py
@@ -0,0 +1,4 @@
+from vehiculo import Automovil
+gtr_r35 = Automovil(315, 10000)
+
+print("El Nissan gtr r35 tiene una velocidad maxima de ", gtr_r35.velocidad_maxima, "kmh y tiene un kilometraje de ", gtr_r35.kilometraje)
